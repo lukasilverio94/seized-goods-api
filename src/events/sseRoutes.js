8eef0de223fa6bd8f6e5b4d9829edbd1cefd9ea2
= <lukasilverio94@gmail.com>
2024-11-21 12:14:14 +0100

    remove unused variable at sse

--- src/events/sseRoutes.js
+++ src/events/sseRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addClient, broadcastToClients } from "./serverSentEvents.js";
+import { addClient } from "./serverSentEvents.js";
 
 const router = express.Router();
 
@@ -7,10 +7,8 @@ router.get("/", (req, res) => {
   const categories = req.query.categories
     ? req.query.categories.split(",").map(Number)
     : [];
-  console.log("categories:", categories);
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Connection", "keep-alive");
 
   res.write("retry: 10000\n\n");
--- src/index.js
+++ src/index.js
@@ -23,13 +23,12 @@ const corsOptions = {
   origin: "http://localhost:5173",
   credentials: true,
   optionsSuccessStatus: 200,
+  allowedHeaders: ["Content-Type"],
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
-
-app.use(cors(corsOptions));
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
