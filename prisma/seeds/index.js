9f189b2fec6aa994b7e35ac948369437f0114b21
= <lukasilverio94@gmail.com>
2025-01-05 13:02:43 +0100

    add seed to script

--- prisma/seeds/index.js
+++ prisma/seeds/index.js
@@ -1,10 +1,12 @@
 import { seedCategories } from "./categories.seed.js";
 import { seedSeizedGoods } from "./seizedGoods.seed.js";
+import { bootstrapAdmin } from "./initialAdmin.js";
 
 async function runSeeds() {
   console.log("Starting database seeding...");
 
   try {
+    await bootstrapAdmin();
     await seedCategories();
     await seedSeizedGoods();
   } catch (error) {
