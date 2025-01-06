9f896f27e08a7e315f09b94bae76e02efc060280
= <lukasilverio94@gmail.com>
2025-01-06 14:52:01 +0100

    fix redis config

--- src/config/redisClient.js
+++ src/config/redisClient.js
@@ -1,6 +1,8 @@
 import { createClient } from "redis";
 
-const redisClient = createClient();
+const redisClient = createClient({
+  url: process.env.REDIS_URL || "http://localhost:6379",
+});
 
 redisClient.on("error", (err) => console.error("Redis Client Error:", err));
 
