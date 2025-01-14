9aa4dd8ead2ff332f0184b0f0859b0da8274a8ef
= <lukasilverio94@gmail.com>
2025-01-15 00:58:17 +0100

    include dotenv on seed script

--- prisma/seeds/index.js
+++ prisma/seeds/index.js
@@ -1,6 +1,8 @@
 import { seedCategories } from "./categories.seed.js";
 import { seedSeizedGoods } from "./seizedGoods.seed.js";
 import { bootstrapAdmin } from "./initialAdmin.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 async function runSeeds() {
   console.log("Starting database seeding...");
