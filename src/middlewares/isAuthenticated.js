9c869f4a4a8555eac58ffbae26c9a91f07f70c72
= <lukasilverio94@gmail.com>
2025-01-15 18:44:36 +0100

    fix isAuthenticated middleware

--- src/middlewares/isAuthenticated.js
+++ src/middlewares/isAuthenticated.js
@@ -2,35 +2,19 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
-const isAuthenticated = (req, res, next) => {
+const isAuthenticated = async (req, res, next) => {
+  const token = req.cookies.accessToken;
+
+  if (!token) {
+    return res.status(403).json({ message: "Unauthorized" });
+  }
+
   try {
-    let token;
-
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer")
-    ) {
-      token = req.headers.authorization.split(" ")[1];
-    }
-    // Fallback: Try to extract token from the cookies
-    else if (req.cookies && req.cookies.refreshToken) {
-      token = req.cookies.refreshToken;
-    }
-
-    // If no token is found, throw an error
-    if (!token) {
-      throw new AppError("Authentication token not found", 403);
-    }
-
-    // Verify the token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    // Attach the user info to the request for downstream use
     req.user = decoded;
-
     next();
   } catch (error) {
-    next(new AppError("Unauthorized: Invalid or missing token", 403));
+    return res.status(401).json({ message: "Invalid or expired access token" });
   }
 };
 
