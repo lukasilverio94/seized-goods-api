4f07a5bb02d742723c53f3aa7a6a8393208bf0e9
= <lukasilverio94@gmail.com>
2025-01-21 16:25:25 +0100

    protect categories route

--- src/routes/categoryItemRoutes.js
+++ src/routes/categoryItemRoutes.js
@@ -5,11 +5,15 @@ import {
 } from "../controllers/categoryItemController.js";
 import { categoryValidationRules } from "../validators/category.js";
 import { handleValidationErrors } from "../middlewares/handleValidationErrors.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import requireRole from "../middlewares/requireRole.js";
 const router = express.Router();
 
 router.get("/", getAllCategories);
 router.post(
   "/",
+  isAuthenticated,
+  requireRole("ADMIN"),
   categoryValidationRules,
   handleValidationErrors,
   createCategory
